498a1769a6fa684262142853854dc761f53d2676
hanluo88 <han_luo@t-online.de>
2021-10-07 10:11:08 +0000

    features selected

--- knnCV_BA_ML.py
+++ knnCV_BA_ML.py
@@ -9,6 +9,9 @@ from xgboost import XGBClassifier
 from xgboost import plot_importance
 from matplotlib import pyplot
 import warnings
+from xgboost.core import Booster
+
+from xgboost.plotting import plot_tree
 
 warnings.filterwarnings("ignore")
 
@@ -64,16 +67,37 @@ pima_features_train, pima_features_test, pima_class_train, pima_class_test = tra
 
 # Jetzt mit XGBoost
 xgmodel = XGBClassifier(eval_metric='error')
-xgmodel.fit(pima_features_train, pima_class_train)
+# xgmodel.fit(pima_features_train, pima_class_train)
 # print(xgmodel) zeigt die Parameter des Classifiers an
 
-xgboosted_prediction = xgmodel.predict(pima_features_test)
+# xgboosted_prediction = xgmodel.predict(pima_features_test)
 
-acc = accuracy_score(pima_class_test, xgboosted_prediction)
-# print("Accuracy: ", acc)
+# acc = accuracy_score(pima_class_test, xgboosted_prediction)
+# print("Accuracy with all Features: ", acc)
 acc_CV = cross_val_score(xgmodel, pima_features, pima_class, cv=10)
-# print(acc_CV)
+print(acc_CV, "Mean-Accuracy with all Features: ", mean(acc_CV))
 # print(mean(acc_CV))
-print(xgmodel.feature_importances_)
-plot_importance(xgmodel)
-pyplot.show()
+# print(xgmodel.feature_importances_)
+# plot_importance(xgmodel)
+# pyplot.show()
+###########################################################################################################################
+###########################################################################################################################
+# Von den 8 Features nehmen ich die 6 mit den höchsten F-Scores. Das sind:
+#pedi, plas, mass, age, pres, preg
+most_important_features = pima_features.loc[:, ['pedi', 'plas', 'mass', 'age', 'pres', 'preg']]
+#Reduziertes Dataset splitten
+# reduced_pima_features_train, reduced_pima_features_test, reduced_pima_class_train, reduced_pima_class_test = train_test_split(
+#     most_important_features, pima_class, test_size=0.2, random_state=7, stratify=pima_class)
+xgmodel_reduced_features = XGBClassifier(eval_metric='error')
+# print(xgmodel_reduced_features)
+# xgmodel_reduced_features.fit(reduced_pima_features_train, reduced_pima_class_train)
+# reduced_xgboosted_prediction = xgmodel_reduced_features.predict(reduced_pima_features_test)
+# acc_reduced = accuracy_score(reduced_pima_class_test, reduced_xgboosted_prediction)
+# print("Accuracy with 6 most relevant Features: ", acc_reduced)
+# print(xgmodel_reduced_features.feature_importances_)
+
+
+# # #Direkt mit CV
+accCV_reduced_features = cross_val_score(xgmodel_reduced_features, most_important_features, pima_class, cv=10)
+print(accCV_reduced_features, "Mean-Accuracy with the 6 most relevant Features: ", mean(accCV_reduced_features))
+
